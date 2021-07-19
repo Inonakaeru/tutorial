4efe6cdbb28506390d84467c4af6e0f8e13559d8
Inonaka, Kaeru <inonaka.kaeru@gmail.com>
2021-07-19 14:59:12 +0900

    added storage

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -11,7 +11,9 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://dsqe37tub5etnor7cryxivzgyy.appsync-api.ap-northeast-1.amazonaws.com/graphql",
     "aws_appsync_region": "ap-northeast-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-fwwl5ignpfcjlgfnpuo53l6f7u"
+    "aws_appsync_apiKey": "da2-fwwl5ignpfcjlgfnpuo53l6f7u",
+    "aws_user_files_s3_bucket": "tutorial-bucket162029-dev",
+    "aws_user_files_s3_bucket_region": "ap-northeast-1"
 };
 
 
